bb9ddb6c863199700647da9004633a1fb59046d0
Martin Tanielian <martin.tanielian@gmail.com>
2023-01-13 18:43:26 -0300

    styles in vercel

--- components/ListHouse/ListItem.tsx
+++ components/ListHouse/ListItem.tsx
@@ -14,7 +14,7 @@ const ListItem = ({ house }: Props) : JSX.Element => {
   const { image, price, direction } = house
   return (
     <>
-      <Grid item md={4} xs={12} className={styles.houseItem}>
+      <Grid item md={4} xs={12} className={styles.houseItem} mb={2}>
         <ImageListItem sx={{width: '90%', cursor: 'pointer'}}  aria-label='areaClick' >
           <img src={`/images/${image}`} alt={`house in ${direction}`} loading="lazy" style={{borderRadius: '10px'}} />
           <ListItemFavorite />
@@ -24,7 +24,7 @@ const ListItem = ({ house }: Props) : JSX.Element => {
           />
         </ImageListItem>
       </Grid>
-      <Grid item xs={12} sx={{ display: { xs: 'flex', md: 'none', justifyContent: 'center'}, mb:2 }}>
+      <Grid item xs={12} sx={{ display: { xs: 'flex', md: 'none', justifyContent: 'center'}, mb:4 }}>
         <Button variant='contained'> SHOW DETAILS </Button>
       </Grid>
     </>
